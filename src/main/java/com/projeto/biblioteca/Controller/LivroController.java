0c6f3dfbe16c32b131882aa46730c1672cad4de7
levybsousa <levybsousa02@gmail.com>
2024-02-28 18:10:42 -0500

    Primeiros passos na classe LivroController

--- src/main/java/com/projeto/biblioteca/Controller/LivroController.java
+++ src/main/java/com/projeto/biblioteca/Controller/LivroController.java
@@ -1,4 +1,40 @@
 package com.projeto.biblioteca.Controller;
 
+import com.projeto.biblioteca.model.entity.Livro;
+import lombok.NoArgsConstructor;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RequestMapping("/livros")
+@RequiredArgsConstructor
+@RestController
 public class LivroController {
+
+    @GetMapping
+    public ResponseEntity<List<Livro>> getAll() {
+        return null;
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Livro> getById(@PathVariable Long Id) {
+        return null;
+    }
+
+    @PostMapping
+    public ResponseEntity<Livro> save(@PathVariable Long Id, @RequestBody Livro livro) {
+        return null;
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Livro> update(@PathVariable Long Id, @RequestBody Livro livro){
+        return null;
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> delete(@PathVariable Long id){
+        return null;
+    }
 }
