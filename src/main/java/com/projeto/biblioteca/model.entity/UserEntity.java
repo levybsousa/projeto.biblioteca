345ba9a1179f14c8f6596abf4cb893efc7de3983
levybsousa <levybsousa02@gmail.com>
2024-02-29 21:56:03 -0500

    Iniciando Repository

--- src/main/java/com/projeto/biblioteca/model.entity/UserEntity.java
+++ src/main/java/com/projeto/biblioteca/model.entity/UserEntity.java
@@ -11,7 +11,7 @@ import java.util.List;
 @Table(name = "usuario")
 @Entity
 @NoArgsConstructor
-public class userEntity {
+public class UserEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
